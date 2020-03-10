21520cfad446eb380a0a2164afe179fae9b0882c
sonia <sonia@DESKTOP-BGUL1JV.station>
2020-03-10 21:44:17 +0100

    Progetto finito

--- Lab01_Parole/src/main/java/it/polito/tdp/parole/FXMLController.java
+++ Lab01_Parole/src/main/java/it/polito/tdp/parole/FXMLController.java
@@ -13,13 +13,17 @@ import javafx.scene.control.TextField;
 public class FXMLController {
 	
 	Parole elenco ;
-
+	long l = System.nanoTime();
+	
     @FXML
     private ResourceBundle resources;
 
     @FXML
     private URL location;
 
+    @FXML
+    private Button btnCancella;
+    
     @FXML
     private TextField txtParola;
 
@@ -31,23 +35,52 @@ public class FXMLController {
 
     @FXML
     private Button btnReset;
+    
+    @FXML
+    private TextArea txtTempi;
 
     @FXML
     void doInsert(ActionEvent event) {
   
+    	String pippo = "";
     	String parolaInserita = txtParola.getText();
     	if(!parolaInserita.contains(" ")) {
     		elenco.addParola(parolaInserita);
     		
-        	txtResult.setText(elenco.getElenco().toString());
+    		for(String s : elenco.getElenco()) {
+    			if(pippo == "") {
+    				pippo = s;
+    			}else {
+    				pippo = pippo + "\n" + s;
+    			}
+    		}
+        	txtResult.setText(pippo);	
     	}
-    	
+        txtTempi.appendText("\n" + l);
     }
 
     @FXML
     void doReset(ActionEvent event) {
     	elenco.reset();
     	txtResult.clear();
+        txtTempi.appendText("\n" + l);
+    }
+    
+    @FXML
+    void doCancella(ActionEvent event) {
+    	
+    	String pippo = "";
+    	String parolaCancellata = txtResult.getSelectedText();
+    	elenco.getElenco().remove(parolaCancellata);
+    	for(String s : elenco.getElenco()) {
+			if(pippo == "") {
+				pippo = s;
+			}else {
+				pippo = pippo + "\n" + s;
+			}
+		}
+    	txtResult.setText(pippo);
+        txtTempi.appendText("\n" + l);
     }
 
     @FXML
